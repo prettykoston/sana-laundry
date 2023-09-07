700ce2df6eb790ace8c0093c7eca346080e0c6d7
Aleksandr Kostin <object.firstname@gmail.com>
2023-09-07 21:25:01 +0300

    Animation van, adaptive mobile service
    Creating animation van driving with GSAP for all breakpoints
    Adapting section-service for mobile
    Adjusting pixel perfect for all breakpoints of section-service

--- scripts/section-service/van-scroll.js
+++ scripts/section-service/van-scroll.js
@@ -1,20 +1,102 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const vanPic = document.querySelector(".section-service__van");
 const serviceSection = document.querySelector(".section-service");
 
 export default function initVan() {
   gsap.registerPlugin(ScrollTrigger);
-  const vanTl = gsap.timeline();
+  const mm = gsap.matchMedia();
 
-  vanTl.to(vanPic, { x: "-25rem" });
+  mm.add(
+    {
+      isDesktop: "(max-width: 120em)",
+      isTabLandscape: "(max-width: 79.6875em)",
+      isTabPortrait: "(max-width: 48em)",
+      isPhone: "(max-width: 20em)",
+    },
+    (context) => {
+      let { isDesktop, isTabLandscape, isTabPortrait, isPhone } =
+        context.conditions;
 
-  ScrollTrigger.create({
-    animation: vanTl,
-    trigger: serviceSection,
-    scrub: true,
-    start: "-=300",
-    end: "+=900",
-  });
+      /* 
+      Desktop 
+      */
+      if (isDesktop) {
+        const vanDesktop = gsap.timeline();
+
+        vanDesktop
+          .to(vanPic, { x: "-20rem" })
+          .to(vanPic, { duration: ".1", rotation: "-0.5" })
+          .to(vanPic, { duration: ".1", rotation: "0" });
+
+        ScrollTrigger.create({
+          animation: vanDesktop,
+          trigger: serviceSection,
+          scrub: 1,
+          start: "center center",
+          end: "+=1700",
+          pin: ".pin-van-animation",
+        });
+      }
+
+      /* 
+      Tablet Landscape Mode
+      */
+      if (isTabLandscape) {
+        const vanTabLandscape = gsap.timeline();
+        vanTabLandscape
+          .to(vanPic, { x: "-5rem" })
+          .to(vanPic, { duration: ".1", rotation: "-0.5" })
+          .to(vanPic, { duration: ".1", rotation: "0" });
+        ScrollTrigger.killAll();
+        ScrollTrigger.create({
+          animation: vanTabLandscape,
+          trigger: serviceSection,
+          start: "top center",
+          end: "+=800",
+        });
+      }
+
+      /* 
+      Tablet Portrait Mode
+      */
+      if (isTabPortrait) {
+        const vanTabPortrait = gsap.timeline();
+
+        vanTabPortrait
+          .to(vanPic, { x: "2rem" })
+          .to(vanPic, { duration: ".1", rotation: "-0.5" })
+          .to(vanPic, { duration: ".1", rotation: "0" });
+
+        ScrollTrigger.killAll();
+        ScrollTrigger.create({
+          animation: vanTabPortrait,
+          trigger: serviceSection,
+          start: "top center",
+          end: "bottom",
+        });
+      }
+
+      /* 
+      Phone
+      */
+      if (isPhone) {
+        const vanPhoneTl = gsap.timeline();
+
+        vanPhoneTl
+          .to(vanPic, { x: "-.5rem" })
+          .to(vanPic, { duration: ".1", rotation: "-0.5" })
+          .to(vanPic, { duration: ".1", rotation: "0" });
+
+        ScrollTrigger.killAll();
+        ScrollTrigger.create({
+          animation: vanPhoneTl,
+          trigger: serviceSection,
+          start: "top top",
+          end: "bottom",
+        });
+      }
+    }
+  );
 }
